05da99228f580f83f34a1409a2ac80ba0b36c4a6
netmee <nk@netmee.com>
2017-02-11 20:01:35 +0100

    https://openclassrooms.com/courses/apprenez-a-programmer-en-python/les-structures-conditionnelles : ok

--- annee-bissextille.py
+++ annee-bissextille.py
@@ -2,11 +2,8 @@
 # -*-coding:Utf-8 -*
 
 annee = input("Saisissez une année : ")
-print(annee)
 
-if annee % 4 == 0:
-  if annee % 100 == 0:
-  else:
-    
+if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
+  print("Année bissextile !")
 else:
-  print("Année non bissextile.")
+  print("Année  non bissextile.")
