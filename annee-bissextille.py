35747ad8ebece39dbbb203c8c08675b41a6cb615
netmee <nk@netmee.com>
2017-02-11 19:36:58 +0100

    initialisation

---
+++ annee-bissextille.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2.7
+# -*-coding:Utf-8 -*
+
+annee = input("Saisissez une année : ")
+print(annee)
+
+if annee % 4 == 0:
+  if annee % 100 == 0:
+  else:
+    
+else:
+  print("Année non bissextile.")
